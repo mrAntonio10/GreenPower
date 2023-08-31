3a77d5437582fe1f33453419c84fd898979fe2c7
marcoroca <mroca@mc4.com.bo>
2023-08-31 18:10:53 -0400

    proyecto spring v0

---
+++ upb/upb/src/main/java/com/upb/upb/UpbGestionApplication.java
@@ -0,0 +1,13 @@
+package com.upb.upb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpbGestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpbGestionApplication.class, args);
+	}
+
+}
---
+++ upb/upb/src/test/java/com/upb/upb/UpbGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upb.upb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpbGestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
